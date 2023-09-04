70dcadec723f346f48f92ff6aae315857101d887
Sanjorika A <143633287+Sanjorika@users.noreply.github.com>
2023-09-05 00:18:45 +0530

    Create greatest integer.cpp

---
+++ 04.09.2023/greatest integer.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,x,y,z;
+    printf("Enter three numbers");
+    scanf("%d %d %d", &a, &b, &c);
+    if(a>b)
+    x=a;
+    else
+    x=b;
+    if (b>c)
+    y=b;
+    else
+    y=c;
+    if(x,y)
+    z=x;
+    else
+    z=y;
+    printf("The greater integer is %d",z);
+    return 0;
+}
