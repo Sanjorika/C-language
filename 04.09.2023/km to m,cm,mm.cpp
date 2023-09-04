fdbd45abe4784f7e3ad02ff43e8428958e8a4cd7
Sanjorika A <143633287+Sanjorika@users.noreply.github.com>
2023-09-04 23:55:02 +0530

    Update km to m,cm,mm.cpp

--- 04.09.2023/km to m,cm,mm.cpp
+++ 04.09.2023/km to m,cm,mm.cpp
@@ -5,10 +5,10 @@ int main()
 	printf ("Enter the kilometer");
 	scanf ("%d", &a);
 	b=a*1000;
-	printf ("The given kilometer is %d m", b);
+	printf ("The given kilometer is %d m\n", b);
 	c=b*100;
-	printf("The given kilometer is %d cm", c);
+	printf("The given kilometer is %d cm\n", c);
 	d=c*10;
-	printf("The given kilometer is %d mm", d);
+	printf("The given kilometer is %d mm\n", d);
 	return 0;
 }
