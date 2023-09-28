acf99a1c30e53979a26494995b9f39ab53a594a6
Sanjorika A <143633287+Sanjorika@users.noreply.github.com>
2023-09-28 16:26:24 +0530

    Update dd mm yy.cpp

--- 05.09.2023/dd mm yy.cpp
+++ 05.09.2023/dd mm yy.cpp
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int d,m,y,a;
-	printf("Enter the date(in the format of dd mm yy)");
+	int d,m,y;
+	printf("Enter the date (in the format of dd mm yyyy)");
 	scanf("%d %d %d", &d, &m, &y);
-	if ((d==31 && (m==1||m=3||m==5||m==7||m==8||m==10))||(d==30 && (m==4||m==6||m==9||m==11)))
-	 {
-	 	a=m+1;
-	 	printf("1 %d %d",a,y);
-	 }
-	else if((y%4==0 && (y%100!=0 || y%400==0)) && d==29 && m==2)
-	{
-		
-	}
-
+	if ((d==31 && (m==1||m==3||m==5||m==7||m==8||m==10))||(d==30 && (m==4||m==6||m==9||m==11))||((y%4==0 && (y%100!=0 || y%400==0)) && d==29 && m==2)||(y%4!=0 && d==28 && m==2))
+	  printf("1 %d %d",m+1,y);
+	else if (d==31 && m==12)
+	  printf("1 1 %d",y+1);
+	else if (((m==1||m==3||m==5||m==7||m==8||m==10) && 1<=d &&d<31)||((m==4||m==6||m==9||m==11)&& 1<=d &&d<30)||((y%4==0 &&(y%100!=0 || y%400==0)) && 1<=d&&d<29 && m==2)||(y%4!=0 && m==2 && 1<=d&&d<28))
+	  printf("%d %d %d",d+1,m,y);
+   else
+     printf("Invalid input");
+return 0;
 }
