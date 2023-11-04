58b41c51eb04fac401a15de8dfcab6ad696ce5ce
Sanjorika A <143633287+Sanjorika@users.noreply.github.com>
2023-11-05 00:17:22 +0530

    Update min, max,avg.cpp

--- 16.10.2023/min, max,avg.cpp
+++ 16.10.2023/min, max,avg.cpp
@@ -1,24 +1,24 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main() 
 {
-  int x,n,i,min=0,max=0,sum=0;
-  printf("Enter the length of the array\n");
+  int x, n, i, min, max, sum;
+  printf("Enter the length of the array");
   scanf("%d", &n);
   int a[n];
-  min=a[0];
+  min = a[0],max=a[0],sum=0;
   printf("Enter the values in the array\n");
-  for(i=0;i<n;i++)
+  for (i = 0; i < n; i++) 
   {
-  	scanf("%d", &a[i]);
-  	if(a[i]<min)
-     min=a[i];
-    if(a[i]>max)
-     max=a[i];
-    sum=sum+a[i];
-    x=sum/n;
+    scanf("%d", &a[i]);
+    if (a[i] < min) 
+      min = a[i];
+    if (a[i] > max) 
+      max = a[i];
+    sum = sum + a[i];
   }
+  x = sum / n; 
   printf("%d is the minimum\n", min);
   printf("%d is the maximum\n", max);
   printf("%d is the average\n", x);
-  return 0;  
+  return 0;
 }
